40fb78cbdc75f8084319b3807164d65969524c58
rojae <rojae@kakao.com>
2022-06-12 18:15:39 +0900

    Feat : Redis 연동완료 (테스트 : RedisRepositoryTest.java)

---
+++ src/main/java/kr/imcf/ott/domain/redis/RAccount.java
@@ -0,0 +1,35 @@
+package kr.imcf.ott.domain.redis;
+
+
+import kr.imcf.ott.common.type.PlatformType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@RedisHash(value = "RAccount", timeToLive = 30)
+public class RAccount {
+    @Id
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private char isAuth;
+
+    private PlatformType platformType;
+
+    private String profileImage;
+
+    private String accessToken;
+
+    private String refreshToken;
+}
---
+++ src/test/java/kr/imcf/ott/sample/RedisRepositoryTest.java
@@ -0,0 +1,52 @@
+package kr.imcf.ott.sample;
+
+import kr.imcf.ott.common.type.PlatformType;
+import kr.imcf.ott.domain.redis.RAccount;
+import kr.imcf.ott.persistence.repository.AccountRedisRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+public class RedisRepositoryTest {
+
+    @Autowired
+    private AccountRedisRepository accountRedisRepository;
+
+    @Test
+    void save() {
+        RAccount account = createNew();
+
+        // 저장
+        accountRedisRepository.save(account);
+
+        // `keyspace:id` 값을 가져옴
+        log.info(accountRedisRepository.findById(account.getId()).get().getId().toString());
+    }
+
+    @Test
+    void count(){
+        // RAccount Entity 의 @RedisHash 에 정의되어 있는 keyspace (people) 에 속한 키의 갯수를 구함
+        long count = accountRedisRepository.count();
+        log.info("Redis Count : {}", count);
+    }
+
+    @Test
+    void delete(){
+        RAccount account = createNew();
+
+        // 삭제
+        accountRedisRepository.delete(account);
+    }
+
+    RAccount createNew(){
+        return new RAccount((long) 1, "rojae", "rojae@kakao.com",
+                '1', PlatformType.IMCF, "",
+                "ACCESSTOKEN", "REFRESHTOKEN");
+    }
+
+
+
+}
