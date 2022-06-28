f1d090155bc3262bb7f12381fd4eb412fc39ca2a
mokacolar <jinyong0826@gmail.com>
2022-06-28 21:17:38 +0900

    회원 정보 조회 및 회원 정보 수정

--- src/main/java/kr/imcf/ott/account/AccountController.java
+++ src/main/java/kr/imcf/ott/account/AccountController.java
@@ -4,10 +4,7 @@ import kr.imcf.ott.common.Message;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,23 +12,28 @@ public class AccountController {
 
     private final AccountService accountService;
 
-    @GetMapping("/account/info")
+    @GetMapping("account/info")
     public ResponseEntity<MyInfoResponse> showMyInfo() {
         // 원래라면 HTTP Header에 존재하는 토큰을 사용해서, 사용자 구분이 가능
         // 현재 토큰 인증 정보 미개발로 인해서, 사용자의 이메일이나 ID를 임의로 넣어서 추출
-        accountService.showMyInfo();
 
-        MyInfoResponse message = MyInfoResponse.builder().code(200).response("조회완료").name("성함")
-                .email("rojae@kakao.com").profileImage("프로필사진").platformType("IMCF").build();
+        //임시 email로 해당 email를 가진 사용자 조회
+        String tempEmail = "rojae@kakao.com";
+
+        MyInfoResponse showMessage = accountService.showMyInfo(tempEmail);
+
+        //builder 패턴, 코드 개선 필요
+        MyInfoResponse message = MyInfoResponse.builder().name(showMessage.getName()).email(showMessage.getEmail()).code(200).response("조회완료").build();
         return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
     @PutMapping("/account/info")
     public ResponseEntity<Message> editMyInfo(@RequestBody MyInfoEditRequest request){
         // 내 정보 수정
-        accountService.editMyInfo();
+        if(accountService.editMyInfo(request))
+            return new ResponseEntity<>(Message.builder().code(200).response("수정되었습니다.").build(), HttpStatus.OK);
+        else
+            return new ResponseEntity<>(Message.builder().code(500).response("수정되지않았습니다.").build(), HttpStatus.OK);
 
-        Message message = Message.builder().code(200).response("수정되었습니다").build();
-        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 }
