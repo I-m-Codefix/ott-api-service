d2bb0f0508b07bf3042e45e714af93818800c58d
rojae <rojae@kakao.com>
2022-07-15 16:29:22 +0900

    Feat : 메일 발송 기능 개발 (SMTP)

--- src/main/java/kr/imcf/ott/Application.java
+++ src/main/java/kr/imcf/ott/Application.java
@@ -1,9 +1,20 @@
 package kr.imcf.ott;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@ServletComponentScan
 @SpringBootApplication
+@EnableJpaAuditing
+@PropertySource({
+        "classpath:application-oauth.properties",
+        "classpath:smtp.properties"
+})
 public class Application {
 
     public static void main(String[] args) {
