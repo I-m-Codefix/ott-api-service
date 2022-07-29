ed60ffce46359e80fd71edf9d3cf744df5957699
rojae <rojae@kakao.com>
2022-07-29 20:57:13 +0900

    Feat : Category Entity 테이블 컬럼 수정

--- src/main/java/kr/imcf/ott/domain/entity/Category.java
+++ src/main/java/kr/imcf/ott/domain/entity/Category.java
@@ -23,11 +23,10 @@ public class Category extends TimeEntity{
     @Column(name = "category_name", nullable = false)
     private String category_name;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Category parent_id;
-
     @Column(name = "use_yn", nullable = false, columnDefinition = "CHAR(1) DEFAULT 'Y'", length = 1)
     private char use_yn = 'Y';
 
+    @Column(name = "parent_id", nullable = false)
+    private long parent_id;
 
 }
