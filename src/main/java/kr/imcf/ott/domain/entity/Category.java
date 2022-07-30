93cc6646da8e8bfd8176c55e1306012725ebf047
rojae <rojae@kakao.com>
2022-07-30 16:19:13 +0900

    Feat : Streaming Entitty 구조 추가

--- src/main/java/kr/imcf/ott/domain/entity/Category.java
+++ src/main/java/kr/imcf/ott/domain/entity/Category.java
@@ -5,7 +5,9 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity(name = "TBL_CATEGORY")
 @Getter
@@ -29,4 +31,6 @@ public class Category extends TimeEntity{
     @Column(name = "parent_id", nullable = false)
     private long parent_id;
 
+    @OneToMany(mappedBy = "id", fetch = FetchType.LAZY)
+    private Set<Streaming> streamings = new HashSet<Streaming>();
 }
